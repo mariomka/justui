3227f716c2f98723043aa81e1125b0e1f9fabc33
Mario Juarez <mario@mjp.one>
2026-04-26 23:05:57 +0200

    Add Vue 3 with @vitejs/plugin-vue in mts renderer config

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>💖 Hello World!</h1>
+  <p>Welcome to your Electron application.</p>
+</template>
+
+<script setup lang="ts">
+console.log('👋 This message is being logged by "App.vue", included via Vite');
+</script>
